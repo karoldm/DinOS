b607a681ed22cae63b23c87ad6b1eadc880b7da0
karol marques <karoldm1579@gmail.com>
2025-02-04 14:11:59 -0300

    feat: cursor pointer

---
+++ Assets/Scripts/Shared/CursorPointer.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CursorPointer : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public Texture2D customCursor;
+    private Vector2 hotspot = Vector2.zero;
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        Debug.Log("hovering");
+        Cursor.SetCursor(customCursor, hotspot, CursorMode.Auto);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
