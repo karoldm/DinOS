cdb4861a035cd3e0e2f69099734d71400fd6510a
karol marques <karoldm1579@gmail.com>
2025-02-18 12:54:34 -0300

    feat: update user

--- Assets/Scripts/Level_three/LevelThreeController.cs
+++ Assets/Scripts/Level_three/LevelThreeController.cs
@@ -294,6 +294,7 @@ public class LevelThreeController : UserController
         }
 
         user.levelThree.score = score;
+        user.levelThree.firstTime = false;
         UpdateUser();
 
         foreach (VerticalLayoutGroup queue in queues)
