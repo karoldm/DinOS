50f8f2a0252680c4c3268cfc180a729658ab35ba
karol marques <karoldm1579@gmail.com>
2025-03-10 18:17:36 -0300

    fix: tooltip click

--- Assets/Scripts/Shared/Tip.cs
+++ Assets/Scripts/Shared/Tip.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
-using UnityEngine.UI;
 using TMPro;
 
 public class Tip : MonoBehaviour, IPointerClickHandler
@@ -11,21 +10,61 @@ public class Tip : MonoBehaviour, IPointerClickHandler
     public GameObject panel;
     private bool showing = false;
 
+    private static List<Tip> allTips = new List<Tip>(); 
+
     void Start()
     {
         TextMeshProUGUI tipText = panel.GetComponentInChildren<TextMeshProUGUI>();
         tipText.text = this.text;
+        allTips.Add(this);
+    }
 
+    void OnDestroy()
+    {
+        allTips.Remove(this);
     }
 
     void Update()
     {
-        
+        if (showing && Input.GetMouseButtonDown(0)) 
+        {
+            if (!IsPointerOverUIObject())
+            {
+                CloseAllTips();
+            }
+        }
     }
 
     public void OnPointerClick(PointerEventData eventData)
     {
-        this.showing = !this.showing;
-        this.panel.SetActive(this.showing);
+        bool wasOpen = showing;
+        CloseAllTips(); 
+
+        if (!wasOpen)
+        {
+            showing = true;
+            panel.SetActive(true);
+        }
+    }
+
+    private static void CloseAllTips()
+    {
+        foreach (Tip tip in allTips)
+        {
+            tip.showing = false;
+            tip.panel.SetActive(false);
+        }
+    }
+
+    private bool IsPointerOverUIObject()
+    {
+        PointerEventData eventData = new PointerEventData(EventSystem.current)
+        {
+            position = Input.mousePosition
+        };
+
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(eventData, results);
+        return results.Count > 0; 
     }
 }
