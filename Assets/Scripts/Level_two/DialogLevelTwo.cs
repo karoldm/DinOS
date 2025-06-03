9759953706a75bde33828344ded8678dda91c030
karolyne marques <karoldm1579@gmail.com>
2025-06-03 15:11:47 -0300

    fix: dialogs

--- Assets/Scripts/Level_two/DialogLevelTwo.cs
+++ Assets/Scripts/Level_two/DialogLevelTwo.cs
@@ -28,7 +28,7 @@ public class DialogLevelTwo : MonoBehaviour
         "Cada voo possui um conjunto de tarefas, e cada tarefa precisa ser realizada no seu próprio terminal (identificado pela cor e pelo ícone da tarefa).",
         "Você pode associar os terminais aos núcleos da CPU, onde cada uma é responsável por executar processos diferentes.",
         "Nossos companheiros podem lhe ajudar a realizar essas tarefas, mas cada um deles só pode realizar uma tarefa de cada vez, e enquanto estiver ocupado com uma tarefa não pode receber outras! Além disso, cada terminal só pode ser usado por um Dino por vez.",
-        "Assim como os espaços da memória RAM podem ser divididos em blocos ou partições para diferentes processos, cada Dino representa um bloco com tamanho fixo. Se atente a essa capacidade quando for atribuir uma tarefa a um Dino.",
+        "Assim como os espaços da memória RAM podem ser divididos em partições para diferentes processos, cada Dino representa uma partição com tamanho fixo. Se atente a essa capacidade quando for atribuir uma tarefa a um Dino.",
         "Muito cuidado com tarefas dependentes! Algumas de nossas tarefas dependem de outras de modo que serão executadas imediatamente em seguida. Essas tarefas são marcadas com uma seta.",
         "Para iniciar, basta arrastar uma tarefa de um dos voos até um Dino, e esperar ele concluir.",
     };
@@ -37,7 +37,7 @@ public class DialogLevelTwo : MonoBehaviour
     private static string[] dinnerProblemTexts =
     {
         "Ops! You are dead(lock)!",
-        "Parece que dois ou mais dinossauros dependem um do terminal do outro para concluir suas tarefas, e desse modo nenhum deles pode avançar! As tarefas precisam ser alocadas com cuidado para não ocorrerem situações como essas, uma vez que os recursos do processador são limitados e atendem um processo por vez, se dois processos passarem a depender um do recurso do outro, o Sistema Operacional estará em um impasse!", 
+        "Parece que dois ou mais dinossauros dependem um do terminal do outro para concluir suas tarefas, e desse modo nenhum deles pode avançar! As tarefas precisam ser alocadas com cuidado para não ocorrerem situações como essas, uma vez que os recursos do processador são limitados e atendem um processo por vez, se dois processos passarem a depender um do recurso do outro, o Sistema Operacional estará em um impasse!",
         "Conquista desbloqueada: Problema do jantar dos filósofos!",
     };
     private LinkedList<string> dinnerProblemDialog = new LinkedList<string>(dinnerProblemTexts);
@@ -50,7 +50,7 @@ public class DialogLevelTwo : MonoBehaviour
 
     private static string[] feedbackTexts =
     {
-        "Ops! O Dino poderia lidar com tarefas mais complexas!\r\nNo aeroporto da memória RAM, cada Dino (bloco de memória) tem uma capacidade específica para realizar tarefas (processos). Delegar uma tarefa pequena a um Dino com grande capacidade é como colocar um avião de carga para transportar uma mala de mão: ineficiente e um desperdício de recursos!",
+        "Ops! O Dino poderia lidar com tarefas mais complexas!\r\nNo aeroporto da memória RAM, cada Dino (partição da memória) tem uma capacidade específica para realizar tarefas (processos). Delegar uma tarefa pequena a um Dino com grande capacidade é como colocar um avião de carga para transportar uma mala de mão: ineficiente e um desperdício de recursos!",
         "No jogo, você precisa garantir que cada Dino receba tarefas adequadas ao seu tamanho. Tarefas grandes devem ser atribuídas a Dinos com maior capacidade, enquanto tarefas menores podem ser executadas por Dinos menores. Assim, o aeroporto (sistema) opera de forma eficiente, sem desperdiçar memória ou deixar processos importantes esperando!"
     };
     private LinkedList<string> feedbackDialog = new LinkedList<string>(feedbackTexts);
